993f0719f3281b4ba3d03ca716430129958ce688
Sandor Torok <sandor.torok@ft.com>
2019-04-12 14:45:04 +0300

    Reworked a bit

--- cmd/hello/hello_world.go
+++ cmd/hello/hello_world.go
@@ -6,9 +6,7 @@ import (
 	"strings"
 )
 
-//HelloWorld prints the text "Hello, world!" on the standard output if there are no command line arguments,
-// otherwise it will print "Hello, <cmd-line-args>"
-func HelloWorld() {
+func main() {
 	if len(os.Args) > 1 {
 		fmt.Printf("Hello, %s!\n", strings.Join(os.Args[1:], " "))
 	} else {
